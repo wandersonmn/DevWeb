ccf8e6440006c11bd331a4174b3cc540491519a1
moons2 <moraes.luan1997@gmail.com>
2022-07-31 17:39:46 -0300

    branch luan created

--- BD/scripts/scripts.sql
+++ BD/scripts/scripts.sql
@@ -7,7 +7,7 @@
 -- INICIALIZACAO
 DROP DATABASE CONSULTAS_ONLINE;
 CREATE DATABASE `CONSULTAS_ONLINE`;
-USE `CONSULTAS_ONLINE`;
+USE `CONSULTAS_ONLINE`; 
 
 -- POSSIVEIS TRATAMENTOS DE ERROS
 --
@@ -116,9 +116,13 @@ INSERT INTO USUARIO (`CPF`,`email`,`senha`,`nome`,`sexo`,`telefone`,`data_nascim
 VALUES ('CPFEHSTRING','paulo@admin.com','adm','Paulo Administrador da Silva','M','5511987676545','2002-02-02');
 
 -- 3 profissionais
-INSERT INTO PROFISSIONAL (`CPF_Profissional`,`area_atuacao`,`especialidade`,`qualificacoes`) VALUES ('98765432100','mecanico','fuselagem','/src/qualy/0243.pdf');
-INSERT INTO PROFISSIONAL (`CPF_Profissional`,`area_atuacao`,`especialidade`,`qualificacoes`) VALUES ('42069621000','medico','computadores','todas');
-INSERT INTO ADM (`CPF_Profissional`,`area_atuacao`,`especialidade`,`qualificacoes`) VALUES ('CPFEHSTRING','professor','redes','a maioria');
+INSERT INTO PROFISSIONAL (`CPF_Profissional`,`area_atuacao`,`especialidade`,`qualificacoes`) 
+VALUES ('98765432100','mecanico','fuselagem','/src/qualy/0243.pdf');
+
+INSERT INTO PROFISSIONAL (`CPF_Profissional`,`area_atuacao`,`especialidade`,`qualificacoes`) 
+VALUES ('42069621000','medico','computadores','todas');
+
+INSERT INTO ADM (`CPF_Adm`) VALUES ('CPFEHSTRING');
 
 -- 1 cliente
 INSERT INTO CLIENTE (`CPF_Cliente`) VALUES ('12345678900');
@@ -129,9 +133,6 @@ VALUES ('12345678900','98765432100','2022-07-25','14:00:00.0000000');
 
 INSERT INTO AGENDAMENTO (`CPF_Cliente`,`CPF_Profissional`,`data`,`hora`) 
 VALUES ('12345678900','42069621000','2022-08-12','13:30:00.0000000');
-
-INSERT INTO AGENDAMENTO (`CPF_Cliente`,`CPF_Profissional`,`data`,`hora`) 
-VALUES ('12345678900','CPFEHSTRING','2022-10-03','13:31:00.0000000');
 
 -- listagem dos horarios de conulstas de um cliente
 SELECT * FROM AGENDAMENTO WHERE CPF_Cliente = '12345678900';
